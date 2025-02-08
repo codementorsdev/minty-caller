fbfc168e470eae42aa0cfd8dcc9cc3e0ea3cc628
codementorsdev <shared.drive.temp@gmail.com>
2025-02-08 16:11:30 +0530

    first commit

---
+++ src/config.js
@@ -0,0 +1,60 @@
+export const messages = {
+  login:
+    "Login as any user on this computer and another user on another computer.",
+  create_session: "Creating a session...",
+  connect: "Connecting...",
+  connect_error:
+    "Something went wrong with the connection. Check internet connection or user info and try again.",
+  login_as: "Logged in as ",
+  title_login: "Choose a user to login with:",
+  title_callee: "Choose users to call:",
+  calling: "Calling...",
+  webrtc_not_avaible: "WebRTC is not available in your browser",
+  no_internet: "Please check your Internet connection and try again"
+};
+
+export const credentials = {
+  appId: 8198,
+  authKey: "EF8108A1-E040-45CD-8A88-3D02483F8531",
+};
+
+export const appConfig = {
+  debug: { mode: 1 },
+  videochat: {
+    //   answerTimeInterval: 30,
+    //   dialingTimeInterval: 5,
+    disconnectTimeInterval: 300,
+    //   statsReportTimeInterval: 5
+  },
+};
+
+export const users = [
+  {
+    id: 13087850,
+    name: "Olivia Brown",
+    login: "olivia.brown88",
+    password: "olivia.brown88",
+    color: "#34ad86"
+  },
+  {
+    id: 13087851,
+    name: "David Lee",
+    login: "dave.lee42",
+    password: "dave.lee42",
+    color: "#077988"
+  },
+  {
+    id: 13087852,
+    name: "Sophia Martinez",
+    login: "sophia.martinez77",
+    password: "sophia.martinez77",
+    color: "#13aaae"
+  },
+  {
+    id: 13087853,
+    name: "Michael Rivera",
+    login: "michael.riv89",
+    password: "michael.riv89",
+    color: "#056a96"
+  }
+];
